6d72e88143dbc01c43eb33b29ce4b4d84afc6e02
Jordi Muñoz <101974385+jordimunozflorensa@users.noreply.github.com>
2024-02-28 17:01:37 +0100

    knn completed

---
+++ github_scrpit.R
@@ -0,0 +1,106 @@
+# limpiar los datos del RStudio -> rm(list=ls())
+# limpiar los plots del RStudio -> dev.off()
+
+rm(list=ls())
+dev.off()
+# cargar base de datos
+setwd("F:/q6/dm")
+dd <- read.table("ObesityWithNA.csv",header=T, sep=",");
+
+dd$Weight[is.na(dd$Weight)] <- 0
+dd$Age[is.na(dd$Age)] <- 0
+
+
+#-------------------------------------------------------------
+# versio no fusionada
+fullvariables<-c(4,6)
+aux<-dd[,fullvariables]
+dim(aux)
+names(aux)
+
+# divide in rows that had missing incomes or not on the target variable to be imputed
+aux1 <- aux[!is.na(dd$Weight),]
+dim(aux1)
+aux2 <- aux[is.na(dd$Weight),]
+dim(aux2)
+
+any(is.na(aux1))
+
+
+#Find nns for aux2
+knn.ing = knn(aux1,aux2,dd$Weight[!is.na(dd$Weight)])
+
+#-------------------------------------------------------------
+# versio fusionada
+
+
+uncompleteVars<-c(5,9,10,13,15,16)
+#auxu<-dd[,uncompleteVars]
+#dim(auxu)
+#names(auxu)
+#better if you sort them by increasing number of missing values
+
+fullVariables<-c(4,6)
+aux<-dd[,fullVariables]
+dim(aux)
+names(aux)
+
+for (k in uncompleteVars){
+  aux1 <- aux[!is.na(dd[,k]),]
+  dim(aux1) 
+  aux2 <- aux[is.na(dd[,k]),]
+  dim(aux2)
+  
+  RefValues<- dd[!is.na(dd[,k]),k]
+  #Find nns for aux2
+  knn.values = knn(aux1,aux2,RefValues)   
+  
+  #CARE: neither aux1 nor aux2 can contain NAs
+  
+  
+  #CARE: knn.ing is generated as a factor. 
+  #Be sure to retrieve the correct values
+  
+  dd[is.na(dd[,k]),k] = as.numeric(as.character(knn.values))
+  fullVariables<-c(fullVariables, k)
+  aux<-dd[,fullVariables]
+}
+
+dim(dd)
+summary(dd)
+
+#dd[dd == ""] <- NA
+#write.csv(dd, "ObesityWithNA.csv")
+
+# resumen de cada variable de la dd (incluye median, quantile...)
+summary(dd)
+
+# barplot -> variables descriptives
+barplot(table(dd$Gender))
+
+# histogram -> variables qualitatives
+d1 <- subset(dd, dd$Gender=="Male")
+d2 <- subset(dd, dd$Gender=="Female")
+hist(dd$Weight, breaks = 10, xlim=c(30,200)) 
+hist(d1$Weight, breaks = 10, xlim=c(30,200))
+hist(d2$Weight, breaks = 10, xlim=c(30,200))
+
+head(dd)
+
+boxplot(dd$Weight~dd$family_history_with_overweight)
+d3 <- subset(dd, dd$family_history_with_overweight=="no")
+d4 <- subset(dd, dd$family_history_with_overweight=="yes")
+par(mfrow=c(1,2))
+hist(d3$Weight, breaks = 10, xlim=c(30,200))
+hist(d4$Weight, breaks = 10, xlim=c(30,200))
+
+#install.packages("plyr") # This package is useful as a data manipulator 
+#install.packages("ggplot2") #This package is useful for visualizing data
+library(plyr)    
+library(ggplot2) 
+
+StackedBar = ggplot(dd, aes(dd$Weight, fill = Gender)) + 
+  geom_bar(stat=dd$Weight fun.y="mean") + 
+  scale_fill_brewer(palette = "Set2") + ggtitle("Peso genero")
+StackedBar
+
